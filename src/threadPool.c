cb36208c5c6010cb73af3eff622b4a49d9179936
emiliocuestaf <emiliocuestaf@gmail.com>
2018-02-22 01:07:46 +0100

    seguimos mejorando pool

--- src/threadPool.c
+++ src/threadPool.c
@@ -22,30 +22,51 @@
 #define BUFFER_SIZE 1000
 
 typedef struct _threadPool{
+	
+	//Este mutex regula el acceso de los hilos a memoria compartida, que es justo esta estructura.
+	pthread_mutex_t sharedMutex;
+
+	//Hemos visto que accept es thread-safe, por eso no se incluye un semaforo/mutex para su acceso
+
 	int numThr;
 	pthread_t* threadList;
+	
 	int listeningSocketDescr;
+	
 	int (*handler_pointer)(char*, char*);
 	long int buffSize;
-	//Hemos visto que accept es thread-safe (nos fiamos 100% de stackoverflow)
-	//int acceptSemId;
+	
 } threadPool;
 
 
 
 void* thread_behaviour(void* args){    
     threadPool* datos = (threadPool*) args;
-    int socket;
+    int socket, listeningSocket, buffSize;
+    int (*handler)(char*, char*);
     char inBuffer[BUFFER_SIZE];
     char outBuffer[BUFFER_SIZE];
+    
 
-    int (*handler)(char*, char*) = datos->handler_pointer;
-
+    if(pthread_mutex_lock(&datos->sharedMutex) != 0){
+    	syslog(LOG_ERR, "Error en thread: Error solicitando mutex");
+    	pthread_exit(NULL);
+    }
+    
+    handler = datos->handler_pointer;
+    listeningSocket = datos->listeningSocketDescr;
+    buffSize = datos->buffSize;
+     
+     if(pthread_mutex_unlock(&datos->sharedMutex) != 0){
+    	syslog(LOG_ERR, "Error en thread: Error liberando mutex");
+    	pthread_exit(NULL);
+    }
+    
     while(1){
-        	socket = accept_connection(datos->listeningSocketDescr);
+        	socket = accept_connection(listeningSocket);
 			if(socket == -1)
 				pthread_exit(NULL);
-			my_receive(socket, inBuffer, datos->buffSize);
+			my_receive(socket, inBuffer, buffSize);
 			handler(inBuffer, outBuffer);
 			my_send(socket, outBuffer);
 			close(socket);
@@ -60,23 +81,58 @@ threadPool* pool_ini(int numThr, int listeningSocketDescr, int buffSize, int(*ha
 	threadPool* pool;
 	sigset_t set;
 	
+	//Reserva de memoria para la estructura del pool y sus campos
 	pool = (threadPool*) malloc (sizeof (threadPool));
+	if(!pool->threadList){
+		syslog(LOG_ERR, "Error en Pool: Error reservando memoria para la estructura pool");
+		return NULL;
+	}
 	pool->numThr = numThr;
 	pool->listeningSocketDescr = listeningSocketDescr;
 	pool->handler_pointer = handler_pointer;
 	pool->buffSize = buffSize;
-	
 	pool->threadList = (pthread_t *)malloc(numThr*sizeof(pthread_t));
+	if(!pool->threadList){
+		syslog(LOG_ERR, "Error en Pool: Error reservando memoria para lista de threads");
+		free(pool);
+		return NULL;
+	}
 
+
+	//Inicializacion del mutex que regula el uso de las zonas compartidas
+	if(pthread_mutex_init(&pool->sharedMutex, NULL) != 0){
+		syslog(LOG_ERR, "Error en Pool: Error inicializando MUTEX");
+		free(pool->threadList);
+		free(pool);
+		return NULL;
+	}
+
+	//Con esta mascara evitamos que los threads "no principales" no se vean afectados por SIGINT
+	//Visto de otra manera, el unico thread que maneja SIGINT es el principal.
     sigemptyset(&set);
     sigaddset(&set, SIGINT);
-    if (pthread_sigmask(SIG_BLOCK, &set, NULL) == 0)
-        syslog(LOG_ERR, "error mascara de bloqueo de SIGINT de threads");
+    if (pthread_sigmask(SIG_BLOCK, &set, NULL) == 0){
+        syslog(LOG_ERR, "Error en Pool: Error mascara de bloqueo de seniales en threads");
+        free(pool->threadList);
+		free(pool);
+        return NULL;
+    }
 
+    //Creacion (e inicio de ejecucion) de los threads
 	for(i = 0; i < numThr; i++){
     	if(pthread_create(&pool->threadList[i], NULL, thread_behaviour, (void*) pool)!= 0){
-    		i--;
+    		syslog(LOG_ERR, "Error en Pool: No se han podido crear todos los threads");
+			free(pool->threadList);
+			free(pool);
+			return NULL;
     	}
+    }
+
+    if (pthread_sigmask(SIG_UNBLOCK, &set, NULL) == 0){
+        syslog(LOG_ERR, "Error en Pool: Error mascara de desbloqueo de seniales en thread principal");
+        free(pool->threadList);
+		free(pool);
+        return NULL;
     }
 	
 	return pool;
